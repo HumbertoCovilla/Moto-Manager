cd2c95eca2cf80bf0d46f7c1acba54264bbd4643
Humberto Covilla <humberto.covilla0725@jala.university>
2026-04-29 10:37:38 -0500

    HU-02: Creacion de las carpetas Services y Repositories, junto con sus archivos correspondientes

---
+++ src/main/java/com/motomanager/moto_manager/services/ProductoService.java
@@ -0,0 +1,19 @@
+package com.motomanager.moto_manager.services;
+
+import com.motomanager.moto_manager.entities.Producto;
+import com.motomanager.moto_manager.repository.ProductoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class ProductoService {
+
+    @Autowired
+    private ProductoRepository
+    productoRepository;
+
+    public List<Producto> listarTodos(){
+        return productoRepository.findAll();
+    }
+}
